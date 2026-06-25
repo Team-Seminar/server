8d9fea0cc1d0cf6f28a562c92cc7425bbe60ee9a
ms4411 <myselftry0o0@gmail.com>
2026-06-25 20:01:29 +0900

    토큰 매니저 클래스 작성 완료

--- src/main/java/com/example/server/global/TokenManager.java
+++ src/main/java/com/example/server/global/TokenManager.java
@@ -1,5 +1,6 @@
 package com.example.server.global;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,14 @@ public class TokenManager {
     private static final String SECRET_KEY_STRING="testsecretkey"; //보안 키
     private static final SecretKey SECRET_KEY= Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes(StandardCharsets.UTF_8)); //암호화
     private static final Long VALID_TIME= 30 * 60 * 1000L; //토큰 허용 시간(30분)
+
     public String createToken(String id, Map<String, Objects> tokenContent){
         Date now=new Date();
         Date expirationTime=new Date(now.getTime()+VALID_TIME);
         if (id==null || id.isEmpty()){
             id= UUID.randomUUID().toString();
         }
-        return Jwts.builder()
+        return Jwts.builder() //토큰 발행
                 .subject(id)
                 .issuedAt(now)
                 .expiration(expirationTime)
@@ -27,5 +29,26 @@ public class TokenManager {
                 .signWith(SECRET_KEY)
                 .compact();
 
+    }
+    protected  Claims getToken(String token){
+        try {
+            // Bearer 접두사 제거
+            if (token != null && token.startsWith("Bearer ")) {
+                token = token.substring(7);
+            }
+
+            // 토큰 서명을 검증하고 내부 데이터(Claims)를 파싱
+            // 만료되었거나 누군가 1글자라도 위조했다면 예외(Exception)가 발생.
+            return Jwts.parser()
+                    //해독기 객체 생성
+                    .verifyWith(SECRET_KEY)
+                    .build()
+                    .parseSignedClaims(token) //서명 및 만료기간 인증. 데이터 복호화
+                    .getPayload(); //토큰에서 페이로드만 반환
+
+        } catch (Exception e) {
+            // 토큰 만료, 서명 불일치, 올바르지 않은 구조 등 모든 검증 실패 시 null 반환
+            return null;
+        }
     }
 }
