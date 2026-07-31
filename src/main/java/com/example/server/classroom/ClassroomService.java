dae2c72487f35e66c049782257286f535e06faab
ms4411 <myselftry0o0@gmail.com>
2026-07-31 21:00:08 +0900

    에러수정 : ai말 Clever Cloud MySQL 애드온의 동시 접속 제한(5개)을 초과, 커넥션 풀 크기 줄여라

--- src/main/java/com/example/server/classroom/ClassroomService.java
+++ src/main/java/com/example/server/classroom/ClassroomService.java
@@ -13,28 +13,31 @@ public class ClassroomService {
     final private ClassroomRepository classroomRepository;
 
     //읽기
-    public Classroom classroomGet(Long classId){
+    public Classroom classroomGet(Long classId) {
         return classroomRepository.findById(classId).orElseThrow();
     }
-    public List<Classroom> classroomGetAll(){
+
+    public List<Classroom> classroomGetAll() {
         return classroomRepository.findAll();
     }
+
     //생성
-    public Classroom classroomCreate(String name){
-        Classroom classroom=new Classroom(name);
+    public Classroom classroomCreate(String name) {
+        Classroom classroom = new Classroom(name);
         classroomRepository.save(classroom);
         return classroom;
     }
 
     //삭제
-    public Classroom classroomDelete(Long classId){
+    public Classroom classroomDelete(Long classId) {
         Classroom classroom = classroomRepository.findById(classId).orElseThrow();
         classroomRepository.removeById(classId);
         return classroom;
     }
+
     //수정
     @Transactional
-    public void classroomUpdate(Long classId, ClassroomStatus status){
+    public void classroomUpdate(Long classId, ClassroomStatus status) {
         classroomRepository.findById(classId)
                 .orElseThrow()
                 .UpdateStatus(status);
