ad2549f8b7d8746eeb15348888fd301ef680d929
ms4411 <myselftry0o0@gmail.com>
2026-09-07 19:25:35 +0900

    feat::자동삭제 로그 추가
    삭제된 예약의 개수를 확인할 수 있도록 반환함

--- src/main/java/com/example/server/reservation/ReservationClear.java
+++ src/main/java/com/example/server/reservation/ReservationClear.java
@@ -1,6 +1,7 @@
 package com.example.server.reservation;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,12 +13,14 @@ import java.time.LocalDateTime;
 @Component
 @EnableScheduling
 @RequiredArgsConstructor
+@Slf4j
 public class ReservationClear {
     private final ReservationRepository reservationRepository;
 
     @Scheduled(cron = "* * 22 * * *")
     @Transactional
     public void cleanReservation(){
-        reservationRepository.deleteAllByCreateAtBefore(LocalDateTime.now());
+        Long cnt=reservationRepository.deleteAllByCreateAtBefore(LocalDateTime.now());
+        log.info("삭제된 예약의 개수: {}", cnt);
     }
 }
