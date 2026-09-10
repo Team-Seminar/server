1ee91312631695d125767f563a0e2688ac445749
ms4411 <myselftry0o0@gmail.com>
2026-09-10 10:11:27 +0900

    Feat: update Student logic

--- src/main/java/com/example/server/student/Student.java
+++ src/main/java/com/example/server/student/Student.java
@@ -2,12 +2,15 @@ package com.example.server.student;
 
 import com.example.server.user.User;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import com.example.server.user.UserRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 =======
+=======
+>>>>>>> Stashed changes
 import jakarta.persistence.*;
 >>>>>>> Stashed changes
 import lombok.AllArgsConstructor;
