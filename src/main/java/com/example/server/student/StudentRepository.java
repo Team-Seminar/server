6f347f59992dc80c7e6ebaea22c1d66c1b8bd02b
ms4411 <myselftry0o0@gmail.com>
2026-09-10 16:53:55 +0900

    fix::구조변경에 따른 에러 수정
    구조변경으로 인해 사라진 메서드 사용으로 인한에러 수정

--- src/main/java/com/example/server/student/StudentRepository.java
+++ src/main/java/com/example/server/student/StudentRepository.java
@@ -2,11 +2,13 @@ package com.example.server.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StudentRepository extends JpaRepository<Student, UUID> {
 
     boolean existsByUser_LoginId(String userLoginId);
-
     boolean existsBySchoolNumber(String schoolNumber);
+
+    Optional<Student> findByUser_LoginId(String userLoginId);
 }
