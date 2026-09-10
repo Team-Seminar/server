26380e8fb85ccc6163c8c8b362f61b8695103c7d
ms4411 <myselftry0o0@gmail.com>
2026-09-10 10:15:57 +0900

    dd

--- src/main/java/com/example/server/student/Student.java
+++ src/main/java/com/example/server/student/Student.java
@@ -3,6 +3,7 @@ package com.example.server.student;
 import com.example.server.user.User;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import com.example.server.user.UserRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,10 +12,11 @@ import jakarta.persistence.Enumerated;
 =======
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 import jakarta.persistence.*;
 >>>>>>> Stashed changes
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
