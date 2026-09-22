fec261490d5b7d403ba66690bc3fdf202ab90b50
ms4411 <myselftry0o0@gmail.com>
2026-09-22 21:40:52 +0900

    feat::LogEntity 작성

--- src/main/java/com/example/server/log/LogEntity.java
+++ src/main/java/com/example/server/log/LogEntity.java
@@ -1,4 +1,28 @@
 package com.example.server.log;
 
-public class LogEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogEntity{
+    @Builder.Default
+    LocalDateTime CreateAt=LocalDateTime.now();
+
+    @Builder.Default
+    @Id
+    UUID id=UUID.randomUUID();
+
+    @Column(nullable = false)
+    String content;
+
+    @Enumerated(value = EnumType.STRING)
+    LogStatus logStatus;
 }
--- src/main/java/com/example/server/log/LogStatus.java
+++ src/main/java/com/example/server/log/LogStatus.java
@@ -1,4 +1,5 @@
 package com.example.server.log;
 
 public enum LogStatus {
+    WARING, ERROR, OK
 }
